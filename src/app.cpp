2a8f0da710eb1d558a1d6259723cdf159327b7ae
Amr Alaa <n1amr1@gmail.com>
2016-12-25 21:18:09 +0200

    initial interface-app

---
+++ src/app.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "bigint/BigInt.h"
+#include "bigint/DecimalHelpers.h"
+using namespace std;
+#define Int BigInt
+
+BigInt extractNumber(string s) {
+  return BigInt::ZERO;
+}

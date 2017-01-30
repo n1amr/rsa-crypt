c233f4cf2e12694a33d52a02e542ac99b090e35f
Amr Alaa <n1amr1@gmail.com>
2017-01-30 05:53:04 +0200

    create first test

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,7 +1,7 @@
-#include "tests.h"
-#include "bits/stdc++.h"
-using namespace std;
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-int main() {
-  runTests();
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
